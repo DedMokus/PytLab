d52dc83e9dd4cf8771d49b278ace9748d0406993
DedMokus <pavllebd1998@mail.ru>
2023-09-08 17:54:50 +0300

    3

--- Lab1/ex3.py
+++ Lab1/ex3.py
@@ -1,42 +1,17 @@
+import re
+
+
 def checkDate(date):
-    spl = [int(x) for x in date.split(".")]
-    #print("spl_date",spl)
-    length = len(spl)
-    #print("in_date",length)
-    if length != 3:
-        #print("False in length")
-        return False
-    elif (0 > spl[0]) or (spl[0] > 32):
-        #print("False in day")
-        return False
-    elif (0 > spl[1]) or (spl[1] > 13):
-        #print("False in month")
-        return False
-    elif (2000 > spl[2]) or (spl[2] > 2100):
-        #print("False in year")
-        return False
-    else:
-        return True
+    datere = '\d\d\.\d\d\.\d{4}'
+    return re.search(datere,date)
 
 
 def checkIP(ip):
-    spl = [int(x) for x in ip.split(".")]
-    #print("spl_ip",spl)
-    length = len(spl)
-    #print("in_ip",length)
-    if length != 4:
-        return False
-    elif (0 >= spl[0]) or (spl[0] >= 255):
-        return False
-    elif (0 >= spl[1]) or (spl[1] >= 255):
-        return False
-    elif (0 >= spl[2]) or (spl[2] >= 255):
-        return False
-    elif (0 >= spl[3]) or (spl[3] >= 255):
-        return False
-    else:
-        return True
-
+    ipv4 = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
+25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
+25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
+25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'''
+    return re.search(ipv4,ip)
 
 def checkStr(date,ip):
     if checkDate(date) and checkIP(ip):
